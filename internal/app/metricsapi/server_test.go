252aaa0ec5cb4f104edcccc3c91e327a43875dfc
nikita <nstegura@inbox.ru>
2023-11-28 23:30:08 +0300

    fix

--- internal/app/metricsapi/server_test.go
+++ internal/app/metricsapi/server_test.go
@@ -28,7 +28,7 @@ func testRequest(t *testing.T, ts *httptest.Server, method, path string) (*http.
 
 func TestUpdateGaugeMetricHandler(t *testing.T) {
 	l := logrus.New()
-	r := repo.New(l)
+	r := repo.New(100, "", false, l)
 	businessLayer := business.New(r, l)
 	server := New(NewConfig(), businessLayer, l)
 	server.configRouter()
